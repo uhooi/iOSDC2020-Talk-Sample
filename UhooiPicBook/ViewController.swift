237edcfbbba942ea6e2c72e9aa04ee82613d7f50
uhooi <uhooi@server.fake>
2020-08-29 15:23:45 +0900

    Create project

---
+++ UhooiPicBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UhooiPicBook
+//
+//  Created by uhooi on 2020/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
